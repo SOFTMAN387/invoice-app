2b5fcfcc8c6edc6dec8265ee95ca778a1aae5b94
softman387 <manishkumarpatna2020@gmail.com>
2023-08-25 15:02:03 +0530

    style changed

--- components/sidebar/SideBar.js
+++ components/sidebar/SideBar.js
@@ -1,17 +1,20 @@
 import React from 'react'
+import Link from 'next/link'
 import Image from 'next/image'
 const SideBar = () => {
   return (
     <>
   <div className='sidebar'>
     <div className='sidebar__container'>
-        <div className='sidebar__header'>
-            <div className='sidebar_logo'>
+          <Link href="/">
+          <div className='sidebar__header'>
+          <div className='sidebar_logo'>
                 <h2>SoftMan</h2>
              </div>
        </div>
+       </Link>
     <div className='sidebar__bottom'>
-        <Image src="/avg.png" alt='er-manish-gupta' width="50" height="50"/>
+        <Image src="/images/prof-pic3.jpg" alt='er-manish-gupta' width="50" height="50"/>
 
     </div>
     </div>
